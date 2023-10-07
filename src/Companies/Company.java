e1e6ae1e40caa158081ee2fa7b63c620cdee7b88
andresmartinroyo <andres.martin@correo.unimet.edu.ve>
2023-10-07 15:22:07 -0400

    :sparkles: push

---
+++ src/Companies/Company.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Companies;
+
+import Rules.CompanyRules;
+import Store.Drive;
+
+/**
+ *
+ * @author Andres
+ */
+public class Company {
+    
+    private CompanyRules rules;
+    private Drive drive;
+    
+    
+}
