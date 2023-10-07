6765be34fd5d7c37ff8b4fa52fdf665fe95abdc1
andresmartinroyo <andres.martin@correo.unimet.edu.ve>
2023-10-07 19:13:33 -0400

    ahora si estan lista y node

--- src/Primitives/List.java
+++ src/Primitives/List.java
@@ -5,10 +5,117 @@
  */
 package Primitives;
 
+import Store.Drive;
+import Worker.Worker;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Andres
  */
 public class List {
-    
+
+    private Node pFirst;
+    private Node pLast;
+    private int size;
+
+    public List() {
+        this.pFirst = null;
+        this.pLast = null;
+        this.size = 0;
+    }
+
+    public List(Node pFirst) {
+        this.pFirst = pFirst;
+        this.pLast = pFirst;
+        this.size = 1;
+    }
+
+    public boolean isEmpty() {
+        return pFirst == null;
+    }
+
+    public void empty() {
+        this.pFirst = null;
+        this.pLast = null;
+        this.size = 0;
+    }
+
+    /*
+    Agragegar a la lista y empieza a trabajar
+     */
+    public void addToList(Worker data) {
+        Node pNew = new Node(data);
+
+        if (this.isEmpty()) {
+            this.pFirst = pNew;
+            this.pLast = pNew;
+        } else {
+            this.pLast.setpNext(pNew);
+            this.pLast = pNew;
+        }
+        this.size++;
+
+    }
+
+    /*
+    Quitar de la lista al ultimo elemento
+     */
+    public void removeLast(Drive drive) {
+        try {
+            drive.getProducerMutex().acquire();
+            drive.getConsumerMutex().acquire();
+            if (this.isEmpty()) {
+                JOptionPane.showMessageDialog(null, "No se puede quitar mas trabajadores de este tipo debido a que no hay");
+            } else {
+                if (this.size == 1) {
+                    this.pFirst.getData().interrupt();
+                    this.empty();
+                } else {
+                    Node pAux = this.pFirst;
+                    for (int i = 0; i < this.size; i++) {
+                        if (pAux.getpNext().equals(this.pLast)) {
+                            this.pLast.getData().interrupt();
+                            this.pLast = pAux;
+                            this.size--;
+                            break;
+                        }
+                        pAux = pAux.getpNext();
+                    }
+                }
+            }
+            drive.getConsumerMutex().release();
+            drive.getProducerMutex().release();
+        } catch (InterruptedException ex) {
+            Logger.getLogger(List.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    public Node getpFirst() {
+        return pFirst;
+    }
+
+    public void setpFirst(Node pFirst) {
+        this.pFirst = pFirst;
+    }
+
+    public Node getpLast() {
+        return pLast;
+    }
+
+    public void setpLast(Node pLast) {
+        this.pLast = pLast;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
 }
--- src/Primitives/Node.java
+++ src/Primitives/Node.java
@@ -5,10 +5,37 @@
  */
 package Primitives;
 
+import Worker.Worker;
+
 /**
  *
  * @author Andres
  */
 public class Node {
+    
+    private Worker data;
+    private Node pNext;
+
+    public Node(Worker data) {
+        this.data = data;
+    }
+
+    public Worker getData() {
+        return data;
+    }
+
+    public void setData(Worker data) {
+        this.data = data;
+    }
+
+    public Node getpNext() {
+        return pNext;
+    }
+
+    public void setpNext(Node pNext) {
+        this.pNext = pNext;
+    }
+    
+    
     
 }
